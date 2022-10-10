6f749c74ab9ead9c0c712c03071f87fef5bca915
theen <theen@LAPTOP-B4CGHGN8>
2022-10-10 22:13:15 +0200

    Este es el paso 6

--- Proyecto1/src/Principal.java
+++ Proyecto1/src/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 		System.out.println("Hola CMEPPS!!!");
 		int i = 0;
 		for(;;) {
-			System.out.println("Número " + i);
+			System.out.println("Número actual: " + i);
 			i++;
 		}
 	}
